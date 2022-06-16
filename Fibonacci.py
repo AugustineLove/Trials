ed171fb14b1ce9770e3103656c4860eac88100e7
AugustineLove <106684513+AugustineLove@users.noreply.github.com>
2022-06-16 18:31:18 +0000

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,13 +1,15 @@
-def fib(n):
+def fib():
+    print("Enter number of list of the series you want ")
+    n=int(input())
     a=0
     b=1
     print(a)
     print(b)
 
-    for i in range (0,n):
+    for i in range (0,n-2):
         c=a+b
         a=b
         b=c
         print(c)
 
-fib(5)
+fib()
